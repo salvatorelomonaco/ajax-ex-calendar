22040fdbcbfc374d1fa755fe1d6da4e19ec4e2da
salvatorelomonaco <lommetto95@gmail.com>
2020-01-09 16:10:43 +0100

    Problema dei giorni della settimana

---
+++ main.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+
+    var template = $('#giorno').html();
+    var templateFunction = Handlebars.compile(template);
+
+    // Imposto una data iniziale
+    var dataIniziale = '2018-01-01';
+    // La faccio diventare attuale con moment
+    var momentIniziale = moment(dataIniziale);
+    // Richiamo le funzioni
+    stampaGiorni(momentIniziale);
+    stampaFeste(momentIniziale);
+
+    $('#next').click(function() {
+        // Aggiungo un mese alla data da visualizzare
+        momentIniziale.add(1, 'Months');
+        stampaGiorni(momentIniziale);
+        stampaFeste(momentIniziale);
+    });
+
+    $('#prev').click(function() {
+        // Aggiungo un mese alla data da visualizzare
+        momentIniziale.subtract(1, 'Months');
+        stampaGiorni(momentIniziale);
+        stampaFeste(momentIniziale);
+    });
+
+    // Creo una funzione che mi stampa tutti i giorni
+    function stampaGiorni(mese) {
+        $('.container-days').empty();
+        // Controllo quanti giorni ci sono nel mese
+        var giorniMese = mese.daysInMonth();
+        // Mi prendo il mese selezionato
+        var meseCorrente = mese.format('MMMM');
+        $('#mese').text(meseCorrente);
+        // Uso il ciclo per clonare i giorni nel calendario
+        for (var i = 1; i <= giorniMese; i++) {
+            // Oggetti che vanno a popolare il template
+            var elementi = {
+                // i corrisponde ogni volta ad un numero da 1 alla fine del mese
+                'numero-giorno': i,
+                'format-day': mese.format('YYYY-MM-') + giornoDueCifre(i)
+            };
+            var htmlFinale = templateFunction(elementi);
+            $('.container-days').append(htmlFinale);
+        };
+
+    };
+
+    // Creo una funzione per stampare i giorni di festa presi tramite l'API
+    function stampaFeste(mese) {
+        $.ajax({
+            'url': 'https://flynn.boolean.careers/exercises/api/holidays',
+            'method': 'GET',
+            'data': {
+                'year': 2018,
+                'month': mese.month()
+            },
+            'success': function(data) {
+                var festività = data.response;
+                for (var i = 0; i < festività.length; i++) {
+                    var dataFesta = festività[i].date;
+                    var nomeFesta = festività[i].name;
+                    $('.container-days div[data-giorno="' + dataFesta + '"]').addClass('festa').append('<span>' + nomeFesta + '</span>');
+                }
+            },
+            'error': function() {
+                alert('Error')
+            }
+        });
+    };
+
+
+    function giornoDueCifre(giorno) {
+        if (giorno < 10) {
+            return '0' + giorno;
+        } else {
+            return giorno;
+        };
+    };
+});
